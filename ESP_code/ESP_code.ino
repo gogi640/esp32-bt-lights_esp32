7050a158b80f305edacac1027819d36c8630982d
Kristian Klišković <kikihd11@gmail.com>
2023-08-24 00:03:17 +0200

    Printing output only

--- ESP_code/ESP_code.ino
+++ ESP_code/ESP_code.ino
@@ -2,20 +2,37 @@
 #include <ArduinoJson.h>
 
 BluetoothSerial *BT;
+char *json = new char[100];
 
 void setup() {
   // put your setup code here, to run once:
   BT = new BluetoothSerial();
   BT->begin("BMW");
   StaticJsonDocument<100> doc;
-  char *json = new char[100];
   Serial.begin(115200);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(BT->available())
+  int i=0;
+  while(BT->available())
   {
-    Serial.print(BT->read());  
+    json[i] =BT->read();
+    i++;
+    delayMicroseconds(100);
+  }
+  if(i!=0)
+  {
+    Serial.println(i);
+    BT->println("DA");
+    for(int j=0;j<i;j++)
+    {
+      if(json[j] < 10) Serial.print("0");
+      Serial.print(json[j],1);
+    }
+    Serial.println();
+    Serial.println("Outputed!");
+   // doThing();
   }
 }
